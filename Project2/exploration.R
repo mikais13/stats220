dd07a9f6a434838fafb83c4682fc9264fb7b400c
mikais13 <mikai.somerville@gmail.com>
2025-04-02 13:04:14 +1300

    Complete Part E - Project 2

--- Project2/exploration.R
+++ Project2/exploration.R
@@ -15,23 +15,132 @@ latest_data <- logged_data %>%
 # show the columns, their names, and a few initial values
 glimpse(latest_data)
 
-mean_inbox_size = latest_data$inbox_size %>% mean(na.rm = TRUE) %>% round(2)
-print(mean_inbox_size)
+# inbox_size summaries
+mean_inbox_size <- latest_data$inbox_size %>%
+  mean(na.rm = TRUE) %>%
+  round(2) %>%
+  print()
 
+min_inbox_size <- latest_data$inbox_size %>%
+  min(na.rm = TRUE) %>%
+  round(2) %>%
+  print()
+
+max_inbox_size <- latest_data$inbox_size %>%
+  max(na.rm = TRUE) %>%
+  print()
+
+median_inbox_size <- latest_data$inbox_size %>%
+  median(na.rm = TRUE) %>%
+  print()
+
+# summary
+number_read <- latest_data %>%
+  filter(str_detect(email_read, "Yes")) %>%
+  summarise(n())
+
+number_emails_replied <- latest_data %>%
+  filter(str_detect(email_reply, "Yes")) %>%
+  summarise(n())
+
+
+# time_to_view summaries
+mean_view_time <- latest_data$time_to_view %>%
+  mean(na.rm = TRUE) %>%
+  round(2) %>%
+  print()
+
+mean_view_time <- latest_data$time_to_view %>%
+  mean(na.rm = TRUE) %>%
+  as.numeric(units = "secs") %>%
+  round(2) %>%
+  print()
+
+max_view_time <- latest_data$time_to_view %>%
+  max(na.rm = TRUE) %>%
+  print()
+
+
+# possible plots
 print(ggplot(data = latest_data) + 
         geom_bar(aes(x = category)))  # categories need to be changed on the plot, especially verification
 
 print(ggplot(data = latest_data) + 
-  geom_bar(aes(x = email_read)))
+  geom_bar(aes(x = email_read,
+               fill = category)))
 
 print(ggplot(data = latest_data) +
-        geom_bar(aes(x = email_reply)))
+        geom_bar(aes(x = email_reply,
+                     fill = category)))
 
 print(ggplot(data = latest_data) + 
-  geom_bar(aes(x = time_to_view))) 
+  geom_bar(aes(x = time_to_view,
+               fill = deleted)))
 
 print(ggplot(data = latest_data) + 
-  geom_bar(aes(x = deleted))) 
+  geom_bar(aes(x = deleted,
+           fill = email_read)) +
+  labs(title = "Portion of Emails that were Deleted by Read Status",
+       x = "Deleted???????",
+       y = "Number of Emails"))
 
-# add labels to the graphs
+# final summary values
+num_categories <- latest_data %>%
+  distinct(category) %>%
+  nrow()
 
+number_of_emails <- latest_data %>% 
+  select(email_read) %>% 
+  summarise(n())
+
+number_emails_read <- latest_data %>%
+  filter(str_detect(email_read, "Yes")) %>%
+  summarise(n())
+
+number_emails_replied <- latest_data %>%
+  filter(str_detect(email_reply, "Yes")) %>%
+  summarise(n())
+
+mean_view_time <- latest_data$time_to_view %>%
+  mean(na.rm = TRUE) %>%
+  as.numeric(units = "mins") %>%
+  round(2)
+
+num_emails_deleted <- latest_data %>%
+  filter(str_detect(deleted, "Yes")) %>%
+  summarise(n())
+
+min_inbox_size <- latest_data$inbox_size %>%
+  min(na.rm = TRUE) %>%
+  round(2)
+
+max_inbox_size <- latest_data$inbox_size %>%
+  max(na.rm = TRUE) %>%
+  round(2)
+
+# final plots
+ggplot(data = latest_data) + 
+  geom_bar(aes(x = category)) +
+  labs(title = "Categories of Emails",
+       x = "Categories",
+       y = "Number of Emails")
+
+ggplot(data = latest_data) + 
+  geom_bar(aes(x = email_read,
+               fill = email_reply)) +
+  labs(title = "Read Status of Emails",
+       x = "Email Read Status",
+       y = "Number of Emails")
+
+ggplot(data = latest_data) +
+  geom_bar(aes(x = time_to_view)) +
+  labs(title = "Time to View each Email",
+       x = "Time to View",
+       y = " Number of Emails")
+
+ggplot(data = latest_data) + 
+  geom_bar(aes(x = deleted,
+               fill = category)) +
+  labs(title = "Were Emails Deleted?",
+       x = "Deletion Status",
+       y = "Number of Emails")
